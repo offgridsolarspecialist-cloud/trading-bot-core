eda0e6eea7adacc5026ba357b65c1163aa830bf9
offgridsolarspecialist-cloud <offgridsolarspecialist@gmail.com>
2026-04-03 10:37:31 -1000

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,20 +1,15 @@
-from http.server import BaseHTTPRequestHandler
-import json
 import requests
 
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        url = "https://futures.kraken.com/derivatives/api/v3/history/executions"
-        params = {
-            "symbol": "PI_XBTUSD"
-        }
+def handler(request):
+    url = "https://futures.kraken.com/derivatives/api/v3/history/executions"
+    params = {
+        "symbol": "PI_XBTUSD"
+    }
 
-        response = requests.get(url, params=params)
-        data = response.json()
+    response = requests.get(url, params=params)
+    data = response.json()
 
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-
-        self.wfile.write(json.dumps(data).encode())
-
+    return {
+        "statusCode": 200,
+        "body": data
+    }
